ef64849397181fb1ea0c3091ce4ba2db17e2c915
lipprodrigues <43422171+lipprodrigues@users.noreply.github.com>
2018-09-26 17:33:34 -0300

    v1.0
    Primeiro Comit do Projeto

---
+++ 1909correcao/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1909correcao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nomeProduto;
+            int precoProduto;
+            int quantidadeProduto;
+            int valorTotal; 
+
+            Console.Write("Digite o nome do Produto: ");
+            nomeProduto = Console.ReadLine();
+
+            Console.Write("Digite o Preço do Produto: ");
+            precoProduto = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("DIgite a QUantidade de Produto: ");
+            quantidadeProduto = Convert.ToInt32(Console.ReadLine());
+
+            valorTotal = quantidadeProduto * precoProduto;
+
+            Console.Clear();
+            Console.WriteLine(" *********** Venda do Produto ***********");
+            Console.WriteLine(" Produto: " + nomeProduto);
+            Console.WriteLine(" Quantidade: " + quantidadeProduto + " x Valor unitario: " + precoProduto);
+            //.toString() -> Converte o numero para o texto. o "C" -> formata para moeda
+            Console.WriteLine(" Total de Venda: " + valorTotal.ToString("C"));
+
+            }
+            catch (Exception)
+            {
+                Console.Clear();
+                Console.WriteLine("Digita numero seu Animal");
+            }
+           
+             Console.ReadKey();
+
+        }
+    }
+}
